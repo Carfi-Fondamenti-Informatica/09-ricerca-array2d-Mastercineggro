4761d2dcb6e85d53aa2905894d52b96134a98b2d
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-12-13 12:27:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include "lib.h"
- using namespace std;
-        int main(){
-        int dim;
-        cin>>dim;
-        float array[dim];
-        for (int i=0; i<dim; i++){
-            cin>>array[i];
+using namespace std;
+
+int main() {
+    char lista[10][20];
+    char target[20];
+    int pos = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            target[j] = ' ';
+            lista[i][j] = ' ';
         }
-        
-        cout<<max(array, dim)<< endl;
-        return 0;
+    }
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+
+    cin >> target;
+    if(cerca(lista,pos,target)){
+        cout << pos;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
