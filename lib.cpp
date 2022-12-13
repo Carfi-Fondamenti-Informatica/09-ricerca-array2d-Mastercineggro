725086c4cd45d1e437375a30dd1b78918bfcbf95
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-12-13 12:25:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include "lib.h"
+using namespace std;
+float max(float array[], int dim){
+  for (int i=0; i<dim; i++){
+        if (array[0]>array[i]){
+        }
+        else{
+            array[0]=array[i];
+        }
+    }
+    return array[0];
+}
